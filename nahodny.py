b7193aa04d1688a0d8d80049be1a131a46134601
Katy <kudlacova123@trebesin.cz>
2026-09-25 13:13:51 +0200

    otazky a json

--- nahodny.py
+++ nahodny.py
@@ -1,1 +1,17 @@
+import json
+
+
 print("hihi")
+
+
+print(f"""
+Na jakou otázku chcete odpověď ? 
+1.Jaký je rok vydaní?
+2.Jaký herci hrají hlavni postavy
+3.Kdo je režisér?
+4.Jak dlouhý je film?
+""")
+
+cislootazky = input("Napište číslo otázky: ")
+
+print(cislootazky)
