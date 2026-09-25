27f9f2ca4ebf07e022cadb271f39b6018814ca97
Katy <kudlacova123@trebesin.cz>
2026-09-25 13:34:05 +0200

    podminky odpovedi

--- nahodny.py
+++ nahodny.py
@@ -12,6 +12,21 @@ Na jakou otázku chcete odpověď ?
 4.Jak dlouhý je film?
 """)
 
-cislootazky = input("Napište číslo otázky: ")
+with open ("interstellar.json","r", encoding="utf-8") as f:
+        interstellar = json.load(f)
+
+cislootazky = int(input("Napište číslo otázky: "))
 
 print(cislootazky)
+
+if cislootazky == 1:
+    print({interstellar["rokvydani"]})
+elif cislootazky == 2:
+      print({interstellar["herci"]})
+elif cislootazky == 3:
+      ({interstellar["reziser"]})
+elif cislootazky == 4:
+      ({interstellar["delka"]})
+else:
+      print("Prosím vyberte platné číslo otázky")
+      
